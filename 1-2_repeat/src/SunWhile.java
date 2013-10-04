9a5ff24a5d1e47f41bf34b5348a22a420106f296
shigero <nakamichi.shigeaki@gmail.com>
2013-10-05 03:39:17 +0900

    誤操作をリカバリ

---
+++ 1-2_repeat/src/SunWhile.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class SunWhile {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("1からnまでの和を求めます。");
+		System.out.println("nの値: ");
+		int n = stdIn.nextInt();
+
+		int sum = 0;
+		int i = 1;
+
+		while (i <= n) {
+			sum += i;
+			i++;
+		}
+		System.out.println("1からnまでの和fは " + sum + " です。");
+	}
+}
+
